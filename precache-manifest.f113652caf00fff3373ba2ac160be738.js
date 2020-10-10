e9fb4103dd8fa76d5343e57b7a58102ea70a22f7
Ladislav Rolnik <rolniklaci@gmail.com>
2020-10-10 23:05:01 +0200

    Updates

--- precache-manifest.f113652caf00fff3373ba2ac160be738.js
+++ precache-manifest.f113652caf00fff3373ba2ac160be738.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "933549cabc43f7c29ac03341e3145730",
+    "revision": "7ae57b1792d20f83be87dea02f4f89d8",
     "url": "/lacirolnikdev/index.html"
   },
   {
-    "revision": "08779f37fa5a61f0022d",
+    "revision": "368ff74672ffbf22d1b7",
     "url": "/lacirolnikdev/static/css/main.5facb584.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/lacirolnikdev/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "08779f37fa5a61f0022d",
-    "url": "/lacirolnikdev/static/js/main.dd04fcd6.chunk.js"
+    "revision": "368ff74672ffbf22d1b7",
+    "url": "/lacirolnikdev/static/js/main.c7fe6f1d.chunk.js"
   },
   {
     "revision": "6a4321240def5ac366fc",
